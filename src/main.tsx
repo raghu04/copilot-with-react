759d2c4d07cec08f97b7bb4f3cbfdecc6da1411e
Raghu S <40663555+raghu04@users.noreply.github.com>
2025-04-01 20:29:30 +0530

    added employee component

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { Home, Register } from "./pages";
+import { Home, Register, Employee } from "./pages";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="register" element={<Register />} />
+          <Route path="employee" element={<Employee />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Employee/index.tsx
@@ -0,0 +1,10 @@
+const Employee = () => {
+  return (
+    <div>
+      <h1>Employee</h1>
+      <p>Welcome to the employee page!</p>
+    </div>
+  );
+};
+
+export default Employee;
